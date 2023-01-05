cb6ed0462629dfaf6c60bf202cd75e85dca0b01c
Aleksandr Bykov <aleksandr.bykov@vseinstrumenti.ru>
2023-01-05 17:40:04 +0300

    hw03_frequency_analysis Частотный анализ

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,38 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordCount struct {
+	word  string
+	count int
+}
+
+func Top10(textForAnalysis string) []string {
+	words := strings.Fields(textForAnalysis)
+
+	wordCounts := make(map[string]int)
+	for _, word := range words {
+		wordCounts[word]++
+	}
+
+	var wordCountsSlice []wordCount
+	for word, count := range wordCounts {
+		wordCountsSlice = append(wordCountsSlice, wordCount{word, count})
+	}
+
+	sort.Slice(wordCountsSlice, func(i, j int) bool {
+		if wordCountsSlice[i].count == wordCountsSlice[j].count {
+			return wordCountsSlice[i].word < wordCountsSlice[j].word
+		}
+		return wordCountsSlice[i].count > wordCountsSlice[j].count
+	})
+
+	var topWords []string
+	for i := 0; i < 10 && i < len(wordCountsSlice); i++ {
+		topWords = append(topWords, wordCountsSlice[i].word)
+	}
+	return topWords
 }
